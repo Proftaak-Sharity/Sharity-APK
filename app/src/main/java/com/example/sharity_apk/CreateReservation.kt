798c4184e82fa51c2397c95d2ab0037e94a62bb9
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-11-30 20:06:25 +0100

    small changes

--- app/src/main/java/com/example/sharity_apk/CreateReservation.kt
+++ app/src/main/java/com/example/sharity_apk/CreateReservation.kt
@@ -1,10 +1,5 @@
 package com.example.sharity_apk
 
-import com.google.android.material.datepicker.MaterialDatePicker
-
 class CreateReservation {
-    val dateRangePicker =
-        MaterialDatePicker.Builder.dateRangePicker()
-            .setTitleText("Select dates")
-            .build()
+
 }
