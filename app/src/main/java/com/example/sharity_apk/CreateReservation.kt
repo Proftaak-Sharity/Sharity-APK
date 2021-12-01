e70876c5d6e669a604adf172482f1cc3988b2625
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-12-01 20:30:55 +0100

    added Create Reservation
    in progress, morgen weer een dag.

--- app/src/main/java/com/example/sharity_apk/CreateReservation.kt
+++ app/src/main/java/com/example/sharity_apk/CreateReservation.kt
@@ -1,5 +1,31 @@
 package com.example.sharity_apk
 
-class CreateReservation {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sharity_apk.databinding.CreateReservationBinding
 
+class CreateReservation : Fragment() {
+
+    private var _binding: CreateReservationBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = CreateReservationBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
