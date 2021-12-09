23316d753d47e2c5987fa561e9c91a2546336f58
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-12-09 10:50:07 +0100

    added buttons, commit to merge master in to develop

--- app/src/main/java/com/example/sharity_apk/CreateReservation.kt
+++ app/src/main/java/com/example/sharity_apk/CreateReservation.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.sharity_apk.databinding.CreateReservationBinding
 
 class CreateReservation : Fragment() {
@@ -27,5 +28,13 @@ class CreateReservation : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonConfirmReservation.setOnClickListener {
+            findNavController().navigate(R.id.action_CreateReservation_to_GetReservationDetails)
+        }
+
+        binding.buttonConfirmReservationPayLater.setOnClickListener {
+            findNavController().navigate(R.id.action_CreateReservation_to_GetReservationDetails)
+        }
     }
 }
