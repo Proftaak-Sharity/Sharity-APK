a72889ac95f6abd235af24a0d689747032d34439
Shabular <43166019+Shabular@users.noreply.github.com>
2021-12-11 21:09:50 +0100

    search_cars is nu te benaderen middels knoppen

--- app/src/main/java/com/example/sharity_apk/SearchResults.kt
+++ app/src/main/java/com/example/sharity_apk/SearchResults.kt
@@ -1,4 +1,53 @@
 package com.example.sharity_apk
 
-class SearchResults {
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sharity_apk.databinding.SearchCarsBinding
+import com.example.sharity_apk.databinding.SearchResultsBinding
+
+class SearchResults : Fragment() {
+
+    private var _binding: SearchResultsBinding? = null
+    private val binding get() = _binding!!
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = SearchResultsBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//      Button bindings:
+//        binding.searchButton.setOnClickListener { findNavController().navigate(R.id.action_SearchCars_to_SearchResults) }
+//        binding.backButton.setOnClickListener { findNavController().navigate(R.id.action_SearchCars_to_AccountOverview) }
+//        binding.buttonMyDetails.setOnClickListener { findNavController().navigate(R.id.action_AccountOverview_to_LoginFragment) }
+//        binding.buttonMyCars.setOnClickListener { findNavController().navigate(R.id.action_AccountOverview_to_LoginFragment) }
+//        binding.buttonMyReservations.setOnClickListener { findNavController().navigate(R.id.action_AccountOverview_to_LoginFragment) }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+
+
+
+
+
 }
