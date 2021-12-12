66dad5ea77abdd9f9a6802d4bee37a383995408c
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-12-12 20:15:11 +0100

    not working but no errors 2with updaqtes from Bart

--- app/src/main/java/com/example/sharity_apk/GetAllReservations.kt
+++ app/src/main/java/com/example/sharity_apk/GetAllReservations.kt
@@ -39,6 +39,7 @@ class GetAllReservations  : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.get_all_reservations, container, false)
     }
+    
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
