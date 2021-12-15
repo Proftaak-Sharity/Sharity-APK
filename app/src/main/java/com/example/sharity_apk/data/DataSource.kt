a4e80c9cc4864eacce1ca67d03394008d6b1750e
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2021-12-15 20:43:28 +0100

    Update DataSource.kt

--- app/src/main/java/com/example/sharity_apk/data/DataSource.kt
+++ app/src/main/java/com/example/sharity_apk/data/DataSource.kt
@@ -1,45 +1,19 @@
 package com.example.sharity_apk.data
 
 import com.example.sharity_apk.R
-import com.example.sharity_apk.model.CarModel
-import com.example.sharity_apk.model.ReservationModel
+import com.example.sharity_apk.Reservation
 
-object DataSource {
-        val cars: List<CarModel> = listOf(
-            CarModel(
-                R.drawable.opel_astra,
-                "Opel",
-            ),
-            CarModel(
-                R.drawable.mazda_mx3,
-                "Mazda",
-            ),
-            CarModel(
-                R.drawable.ferrari_testarossa,
-                "Ferrari",
-            ),
-            CarModel(
-                R.drawable.seat_leon,
-                "Seat",
-            ),
-            CarModel(
-                R.drawable.volvo_v40,
-                "Volvo",
-            ),
-            CarModel(
-                R.drawable.suzuki_swift,
-                "Suzuki",
+class Datasource {
+    fun loadReservations(): List<Reservation> {
+        return listOf<Reservation>(
+
+            Reservation(R.string.reservation1),
+            Reservation(R.string.reservation2),
+            Reservation(R.string.reservation3),
+            Reservation(R.string.reservation4),
+            Reservation(R.string.reservation5),
+            Reservation(R.string.reservation6),
+            Reservation(R.string.reservation7)
             )
-        )
-        val reservations: List<ReservationModel> = listOf(
-            ReservationModel(
-                "reservering 1"
-            ),
-            ReservationModel(
-                "reservering 2"
-            ),
-            ReservationModel(
-                "reservering 3"
-            )
-        )
+}
 }
