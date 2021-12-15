a4807d0686fececd4b5c590a163ea2cb64a3a8f9
Shabular <43166019+Shabular@users.noreply.github.com>
2021-12-15 11:59:05 +0100

    working recyclerview
    working recyclerview

--- app/src/main/java/com/example/sharity_apk/data/DataSource.kt
+++ app/src/main/java/com/example/sharity_apk/data/DataSource.kt
@@ -1,34 +1,34 @@
 package com.example.sharity_apk.data
 
 import com.example.sharity_apk.R
-import com.example.sharity_apk.model.CarModel
+import com.example.sharity_apk.Reservation
+import com.example.sharity_apk.model.SearchedCar
 
-object DataSource {
-        val cars: List<CarModel> = listOf(
-            CarModel(
-                R.drawable.opel_astra,
-                "Opel",
-            ),
-            CarModel(
-                R.drawable.mazda_mx3,
-                "Mazda",
-            ),
-            CarModel(
-                R.drawable.ferrari_testarossa,
-                "Ferrari",
-            ),
-            CarModel(
-                R.drawable.seat_leon,
-                "Seat",
-            ),
-            CarModel(
-                R.drawable.volvo_v40,
-                "Volvo",
-            ),
-            CarModel(
-                R.drawable.suzuki_swift,
-                "Suzuki",
+class Datasource {
+    fun loadReservations(): List<Reservation> {
+        return listOf<Reservation>(
+
+            Reservation(R.string.reservation1),
+            Reservation(R.string.reservation2),
+            Reservation(R.string.reservation3),
+            Reservation(R.string.reservation4),
+            Reservation(R.string.reservation5),
+            Reservation(R.string.reservation6),
+            Reservation(R.string.reservation7)
             )
+    }
+
+    fun loadSearchedCars(): List<SearchedCar> {
+        return listOf<SearchedCar>(
+
+            SearchedCar(R.string.reservation1),
+            SearchedCar(R.string.reservation2),
+            SearchedCar(R.string.reservation3),
+            SearchedCar(R.string.reservation4),
+            SearchedCar(R.string.reservation5),
+            SearchedCar(R.string.reservation6),
+            SearchedCar(R.string.reservation7)
         )
+    }
 
 }
