ba1b90f2fd64dd96b6cc144dca5aefe5c03c33fa
RobvdHorst <71100038+RobvdHorst@users.noreply.github.com>
2021-12-18 10:37:40 +0100

    welkomtext

--- app/src/main/java/com/example/sharity_apk/AccountOverview.kt
+++ app/src/main/java/com/example/sharity_apk/AccountOverview.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sharity_apk.config.SharityPreferences
 import com.example.sharity_apk.databinding.AccountOverviewBinding
+import com.example.sharity_apk.model.CustomerModel
 import com.example.sharity_apk.service.CustomerApiService
 import com.example.sharity_apk.service.ServiceGenerator
 import kotlinx.coroutines.launch
@@ -34,6 +37,9 @@ class AccountOverview : Fragment() {
         val preferences = SharityPreferences(requireContext())
         val serviceGenerator = ServiceGenerator.buildService(CustomerApiService::class.java)
 
+        val tvTitle: TextView = binding.title
+        tvTitle.text = getString(R.string.welcome, "${preferences.getFirstName()}")
+//
 //      Button bindings:
         binding.buttonMakeReservation.setOnClickListener { findNavController().navigate(R.id.action_AccountOverview_to_SearchCars) }
         binding.buttonMyDetails.setOnClickListener { findNavController().navigate(R.id.action_AccountOverview_to_GetCustomerDetails) }
