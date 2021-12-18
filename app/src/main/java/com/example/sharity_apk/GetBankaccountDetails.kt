c0dfeaff3be653903f83e8071abce66845fefacc
RobvdHorst <71100038+RobvdHorst@users.noreply.github.com>
2021-12-18 11:02:49 +0100

    drivers license working

--- app/src/main/java/com/example/sharity_apk/GetBankaccountDetails.kt
+++ app/src/main/java/com/example/sharity_apk/GetBankaccountDetails.kt
@@ -6,7 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.sharity_apk.config.SharityPreferences
 import com.example.sharity_apk.databinding.GetBankaccountDetailsBinding
+import com.example.sharity_apk.service.CustomerApiService
+import com.example.sharity_apk.service.ServiceGenerator
 
 class GetBankaccountDetails: Fragment() {
 
